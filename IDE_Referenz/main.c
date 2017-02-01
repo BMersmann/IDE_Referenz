96c58d444307ed772d0da4cdb36f9795f0af83ee
bam <b_mersmann@yahoo.de>
2017-02-01 19:09:20 +0100

    initial check in

---
+++ IDE_Referenz/main.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: 01.02.2017
+ *      Author: bam
+ */
+
+
+#include <stdio.h>
+int main()
+{
+int num, count, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    // for loop terminates when n is less than count
+    for(count = 1; count <= num; ++count)
+    {
+        sum += count;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
